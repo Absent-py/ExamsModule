7e2a731f02482700ce6f42ff2c7611fb97399eb3
Sergey <sergeuses1397gubarev@gmail.com>
2023-02-17 13:54:17 +0300

    chore(project): EM-1 cleaned up the code

---
+++ src/components/Preview.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <h1 class="mb-5">Exams Module</h1>
+    <v-card class="card">
+      <p>Данный модуль предназначен для создания и прохождения тестирований, обработки результатов, а также их импорт и экспорта. Проект выполнен на Фреймворке Vue.js 3 + Vite + TS + Vuetify.</p>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Preview"
+}
+</script>
+
+<style>
+.card {
+  max-width: 70%;
+  margin: auto;
+}
+</style>
