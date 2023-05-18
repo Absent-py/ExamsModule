da43f6b7cf1987e47298aac7507f7f54a32510e5
Sergey <sergeuses1397gubarev@gmail.com>
2023-05-18 19:11:47 +0300

    small tweak

--- src/views/Teacher/ExamView.vue
+++ src/views/Teacher/ExamView.vue
@@ -7,14 +7,15 @@
         <v-container>
             <h1>{{ getQuestion().text }}</h1>
             <v-row class="mt-3" style="text-align: start;" v-for="item in getQuestion().answers">
-                <v-btn @click="answers[item.id - 1] = !answers[item.id - 1]" class="btn" :color="`${buttonColor(answers[item.id - 1])}-lighten-3`"></v-btn>
+                <v-btn @click="response[index].answers[item.id - 1] = !response[index].answers[item.id - 1]" class="btn" :color="`${buttonColor(response[index].answers[item.id - 1])}-lighten-3`"></v-btn>
                 <h3 class="pt-3 pl-2">{{ item.text }}</h3>
             </v-row>
         </v-container>
 
         <v-container>
-            <v-btn class="mr-10" @click="previousQuestion">Назад</v-btn>
+            <v-btn v-if="this.index !== 0" class="mr-10" @click="previousQuestion">Назад</v-btn>
             <v-btn v-if="this.index + 1 !== this.maxIndex" class="ml-10" @click="nextQuestion">Следующий вопрос</v-btn>
+            <v-btn v-if="this.index + 1 == this.maxIndex" class="ml-10" @click="finishExam">Завершить</v-btn>
         </v-container>
 
         <v-container>
@@ -44,6 +45,23 @@ export default {
         response: []
     }),
     methods: {
+        setAnswers () {
+            let response = []
+            let answers = []
+            for (let i = 0; i < this.questions.length; i += 1) {
+                for (let j = 0; j < this.questions[i].answers.length; j += 1) {
+                    answers.push(false)
+                }
+                response.push({
+                    id: i,
+                    answers: answers
+                })
+
+                answers = []
+            }
+
+            return response
+        },
         setTestExam() {
             const data = state.getTest()
 
@@ -52,6 +70,8 @@ export default {
             this.author = data.author
             this.subject = data.subject
             this.questions = data.questions
+
+            this.response = this.setAnswers()
         },
         getQuestion() {
             if (this.updated == false) {
@@ -66,20 +86,17 @@ export default {
             return this.questions[this.index]
         },
         nextQuestion() {
-            this.response.push({
-                id: this.index,
-                answers: this.answers
-            })
-
-            this.answers = []
-            this.updated = false
-
             if (this.index + 1 < this.maxIndex) {
                 this.index += 1
             }
         },
         previousQuestion() {
-            this.index -= 1
+            if (this.index > 0) {
+                this.index -= 1
+            }
+        },
+        finishExam() {
+            return this.response
         },
         buttonColor(bool) {
             if (bool) {
